fe363e936d35c33872fd929f68839330b0f096a6
kyle parks <kcparks1234@gmail.com>
2025-09-26 20:37:10 -0700

    Add save/load to CSV file.

--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.*;
 
 public class ExpenseTracker {
     private static ArrayList<Expense> expenses = new ArrayList<>();
@@ -7,14 +8,15 @@ public class ExpenseTracker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice;
+        loadExpenses();
 
         do {
             System.out.println("\n--- Expense Tracker ---");
             System.out.println("1. Add Expense");
             System.out.println("2. View All Expenses");
             System.out.println("3. Show Totals");
-            System.out.println("2. Save & Exit");
-            System.out.println("2. Choice: ");
+            System.out.println("4. Save & Exit");
+            System.out.println("5. Choice: ");
 
             while (!sc.hasNextInt()) {
                 System.out.print("Enter a number 1-4 ");
@@ -27,7 +29,7 @@ public class ExpenseTracker {
                 case 1: addExpense(sc); break;
                 case 2: viewExpenses(); break;
                 case 3: showTotals(); break;
-                case 4: System.out.println("Goodbye!"); break;
+                case 4: saveExpenses(); System.out.println("Goodbye!"); break;
                 default: System.out.println("Invalid choice.");
             }
         } while (choice != 4);
@@ -66,4 +68,33 @@ public class ExpenseTracker {
         for (Expense e : expenses) total += e.getAmount();
         System.out.println("Total expenses: $" + String.format("%.2f", total));
     }
+
+    private static void saveExpenses() {
+        try (PrintWriter pw = new PrintWriter(new FileWriter("expenses.csv"))) {
+            for (Expense e : expenses) {
+                pw.println(e.getDate() + "," + e.getAmount());
+            }
+        } catch (IOException ex) {
+            System.out.println("Error saving file" + ex.getMessage());
+        }
+    }
+
+    private static void loadExpenses() {
+    File f = new File("expenses.csv");
+    if (!f.exists()) return;
+
+    try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] parts = line.split(",");
+            if (parts.length != 3) continue;
+            String date = parts[0];
+            String category = parts[1];
+            double amount = Double.parseDouble(parts[2]);
+            expenses.add(new Expense(amount, category, date));
+        }
+    } catch (IOException ex) {
+        System.out.println("Error reading file: " + ex.getMessage());
+    }
+  }
 }
